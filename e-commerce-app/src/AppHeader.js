893e1fcf259ff519b308e4dea3f0175ff1139046
Liam Rogers <LiamRogersUK@gmail.com>
2023-12-12 12:17:40 +0000

    Add divs to headerTop, Generate button in headerBottom
     new divs within headerTop: storeName and storeCart

--- e-commerce-app/src/AppHeader.js
+++ e-commerce-app/src/AppHeader.js
@@ -3,7 +3,15 @@ import './AppHeader.css'
 function AppHeader() {
     return (
         <div className="headerBody">
-
+            <div className='headerTop'>
+                <div className='storeName'>
+                    <p>-Store Name-</p>
+                </div>
+                <div className='storeCart'>Cart</div>
+            </div>
+            <div className='headerBottom'>
+                <button className='headerButton'>TEST1</button>
+            </div>
         </div>
     )
 }
